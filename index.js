238cb606c074356b429051b8aaa0fd6985a07c80
jungwoo3490 <jungwoo3490@naver.com>
2025-01-29 22:09:50 +0900

    feat: add cron job to send message at 23:00

--- index.js
+++ index.js
@@ -5,33 +5,38 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
-const getToday = () => {
+const getTime = () => {
   const now = new Date();
-  return `${now.getFullYear()}.${now.getMonth() + 1}.${now.getDate()}`;
+  return `${now.getFullYear()}.${
+    now.getMonth() + 1
+  }.${now.getDate()} ${now.getHours()}:${now.getMinutes()}`;
 };
 
 client.once("ready", () => {
   console.log("이정우 분신 1호 재부팅");
 
-  cron.schedule(
-    "0 22 * * *",
-    async () => {
-      try {
-        const channel = await client.channels.fetch(process.env.CHANNEL_ID);
+  const sendMessage = async () => {
+    try {
+      const channel = await client.channels.fetch(process.env.CHANNEL_ID);
 
-        if (channel) {
-          await channel.send("아름아 도윤아 공부하자~~~!!");
-          console.log(`${getToday()} 공부시키기 성공했어요`);
-        }
-      } catch (error) {
-        console.error(`${getToday()} 공부시키기 실패했어요: `, error);
+      if (channel) {
+        await channel.send("아름아 도윤아 공부하자~~~!!");
+        console.log(`${getTime()} 공부시키기 성공했어요`);
       }
-    },
-    {
-      scheduled: true,
-      timezone: "Asia/Seoul",
+    } catch (error) {
+      console.error(`${getTime()} 공부시키기 실패했어요: `, error);
     }
-  );
+  };
+
+  cron.schedule("0 22 * * *", sendMessage, {
+    scheduled: true,
+    timezone: "Asia/Seoul",
+  });
+
+  cron.schedule("0 23 * * *", sendMessage, {
+    scheduled: true,
+    timezone: "Asia/Seoul",
+  });
 });
 
 client.login(process.env.BOT_TOKEN);
